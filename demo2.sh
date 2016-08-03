61037e79d85dff4c6ed8e801b9031b8c75176ab4
Vithin Kanganda Prakash <vithin.kanganda@smartims.com>
2016-08-03 19:08:05 +0530

    hi

---
+++ demo2.sh
@@ -0,0 +1,8 @@
+  #!/bin/bash
+      
+          echo -n "Enter the first number : "
+	      read num1
+	          echo -n "Enter the second number : "
+		      read num2
+		          sum=`expr $num1 + $num2`
+			      echo "sum of two value is $sum"
